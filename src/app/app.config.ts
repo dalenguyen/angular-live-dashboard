aca8f3090f1a7f3d35d1a8a07ad55e59d85c6e90
Dale Nguyen <dale@dalenguyen.me>
2025-01-22 00:12:20 -0500

    chore: add base href to config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,9 +5,11 @@ import {
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { APP_BASE_HREF } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    [{ provide: APP_BASE_HREF, useValue: import.meta.env.BASE_URL || '/' }],
     provideExperimentalZonelessChangeDetection(),
     provideClientHydration(),
     provideRouter(routes),
